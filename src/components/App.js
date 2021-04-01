37eb70915605eaa54731aacbf459fab3551fca54
flipxonsoft <parthmalakar4@gmail.com>
2021-04-02 00:01:11 +0530

    fetched posts from api to redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App">Welcome Codeial</div>;
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/posts';
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    console.log("props :", this.props);
+    return (
+      <div>
+        Welcome to Codeial
+      </div>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    posts: state.posts,
+  };
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
 
 
 const store = configureStore();
-console.log("store.getState(): ", store.getState());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
