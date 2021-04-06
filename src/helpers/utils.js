33657467f050b80933343169d3f53c69ab6edf50
flipxonsoft <parthmalakar4@gmail.com>
2021-04-07 03:19:42 +0530

    Logging user in - III

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -5,7 +5,7 @@ export function getFormBody (params) {
         let encodedKey = encodeURIComponent(property); // 'user name => 'user%20name'
         let encodedValue = encodeURIComponent(params[property]); // parth 123 => 'parth%20123'
 
-        formBody(encodedKey + '=' + encodedValue);
+        formBody.push(encodedKey + '=' + encodedValue);
     }
 
     return formBody.join('&'); // 'username=parth&password=123'
