99dd362db65fb11e3a15ee641833cda6befe2d1a
flipxonsoft <parthmalakar4@gmail.com>
2021-04-22 12:33:42 +0530

    Liking post: actions and reducers

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -4,7 +4,7 @@ export const APIUrls = {
   login: () => `${API_ROOT}/users/login`,
   signup: () => `${API_ROOT}/users/signup`,
   editProfile: () => `${API_ROOT}/users/edit`,
-  fetchPosts: (page = 1, limit = 25) =>
+  fetchPosts: (page = 1, limit = 15) =>
     `${API_ROOT}/posts?page=${page}&limit=${limit}`,
   userProfile: (userId) => `${API_ROOT}/users/${userId}`,
   userFriends: () => `${API_ROOT}/friendship/fetch_user_friends`,
@@ -14,4 +14,6 @@ export const APIUrls = {
     `${API_ROOT}/friendship/remove_friendship?user_id=${userId}`,
   createPost: () => `${API_ROOT}/posts/create`,
   createComment: () => `${API_ROOT}/comments/`,
+  toggleLike: (id, likeType) =>
+    `${API_ROOT}/likes/toggle?likeable_id=${id}&likeable_type=${likeType}`,
 };
